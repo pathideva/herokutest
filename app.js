78faf823346006b73c0d8ff52c62cc54d0f28027
pathideva <pathideva@gmail.com>
2017-12-26 13:36:17 +0530

    changed 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
  
 app.get('/', function(req, res) {
-  res.send('Every day in every way I\'m serving more requests');
+  res.send('Welcome to NOA integration platform');
 });
  
 var port = Number(process.env.PORT || 5000);
